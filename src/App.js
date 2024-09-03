b8a2ad79fa4c02faaaa498970ac68e64ec1fd99e
Abdullah Azam Khan <ranaabdullahazam102@gamil.com>
2024-09-03 16:06:48 +0500

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,44 @@
-import './App.css';
+import "./App.css";
+import Navbar from "./Components/Navbar/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import men_banner from "./Components/Assests/banner_mens.png";
+import women_banner from "./Components/Assests/banner_women.png";
+import kid_banner from "./Components/Assests/banner_kids.png";
+import ShopCategory from "./pages/ShopCategory";
+import LoginSignup from "./pages/LoginSignup";
+import Cart from "./pages/Cart";
+import Product from "./pages/Product";
+import Shop from "./pages/Shop";
 
 function App() {
   return (
-    <div className="App">
-  
+    <div>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Shop />} />
+          <Route
+            path="/mens"
+            element={<ShopCategory banner={men_banner} category="men" />}
+          />
+          <Route
+            path="/womens"
+            element={<ShopCategory banner={women_banner} category="women" />}
+          />
+          <Route
+            path="/Kids"
+            element={<ShopCategory banner={kid_banner} category="kid" />}
+          />
+
+          <Route path="/product" element={<Product />}>
+            <Route path="productId" element={<Product />} />
+          </Route>
+
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/login" element={<LoginSignup />} />
+        </Routes>
+        {/* <Footer/> */}
+      </BrowserRouter>
     </div>
   );
 }
